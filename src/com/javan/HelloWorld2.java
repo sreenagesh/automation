3e7350d437172bbab7920780dd74a767b3d4bfbb
sreenageshu <sreenageshu@gmail.com>
2018-02-08 19:05:58 -0500

    hello world changed

--- src/com/javan/HelloWorld2.java
+++ src/com/javan/HelloWorld2.java
@@ -5,7 +5,7 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		System.out.println("started hello world!!");
 		for(int i=1;i<=10;i++) {
-			System.out.println(i);
+			System.out.println("omg:"+i);
 		}
 
 	}
